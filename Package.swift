bfc780af6c7cb263da9e371f2186798a7e50e175
Pascal Friedrich <pascal.friedrich@brickmakers.de>
2020-01-16 13:52:49 +0100

    Change name of SPM Package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "photo-editor",
+    name: "iOSPhotoEditor",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
